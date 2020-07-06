9f3d086d3b8e06964a1415556d8cb2392d523842
jcable <julian.cable@yahoo.com>
2020-07-06 11:29:46 +0100

    add pv10 for clips

--- index.js
+++ index.js
@@ -59,47 +59,67 @@ const transport = async (operation, s3Location, pid) => {
   }
 }
 
+/*
+2020-07-05T15:29:40.969Z d6908d0f-128c-40dd-ba49-c67fa675849f INFO Profile ID was not pips-map_id-av_pv13_pa4 
+{
+  "content_version_id":"pips-pid-p08jxw47",
+  "drm":"none",
+  "media_asset_id":"pips-pid-p08jxwcm",
+  "sequence_stamp":1593962969001000,
+  "profile_id":"pips-map_id-av_pv10_pa4",
+  "uri":"s3://livemodavdistributionresources-distributionbucket-182btg2y28f33/av_pv10_pa4/modav/bUnknown-99ae4cde-ecc1-4a12-b2dd-71d336ba3f55_p08jxw47_cUnknown_1593962904250.mp4",
+  "last_updated":"2020-07-05T15:29:40.147Z",
+  "event_name":"INSERT"
+}
+*/
+
 exports.handler = async (event, context) => {
   // console.log("Received event:", JSON.stringify(event, null, 2));
-  const wantedProfileId = process.env.PROFILE_ID
   const message = event.Records[0].Sns.Message
   const sns = JSON.parse(message)
   const profileId = sns.profile_id
-  if (profileId === wantedProfileId) {
-    const contentVersionId = sns.content_version_id
-    const pid = contentVersionId.split('pips-pid-')[1]
-    try {
-      const response = await appw.get('version', pid)
-      const link = response.pips.version.version_of.link
-      let masterBrand = ''
-      switch (link.rel.split('pips-meta:')[1]) {
-        case 'clip': {
+  const contentVersionId = sns.content_version_id
+  const pid = contentVersionId.split('pips-pid-')[1]
+  try {
+    const response = await appw.get('version', pid)
+    const link = response.pips.version.version_of.link
+    let masterBrand = ''
+    switch (link.rel.split('pips-meta:')[1]) {
+      case 'clip': {
+        const wantedProfileId = 'pips-map_id-av_pv10_pa4'
+        if (profileId === wantedProfileId) {
           const clip = await appw.get('clip', link.pid)
           masterBrand = clip.pips.master_brand_for.master_brand.mid
+          console.log(JSON.stringify(clip.pips))
           console.log('Clip Master brand is', masterBrand)
+        } else {
+          console.log('Profile ID was not ' + wantedProfileId, message)
         }
-          break
-        case 'episode': {
+      }
+        break
+      case 'episode': {
+        const wantedProfileId = process.env.PROFILE_ID
+        if (profileId === wantedProfileId) {
           const episode = await appw.get('episode', link.pid)
           masterBrand = episode.pips.master_brand_for.master_brand.mid
           console.log('Episode Master brand is ', masterBrand)
-        }
-          break
-        default: // DO NOTHING
-      }
-      if (wantedMasterBrands.includes(masterBrand)) {
-        console.log('wanted masterbrand')
-        try {
-          await transport(sns.event_name, sns.uri, pid)
-        } catch (error) {
-          console.log(error)
+        } else {
+          console.log('Profile ID was not ' + wantedProfileId, message)
         }
       }
-    } catch (error) {
-      console.log('Error! ', error)
+        break
+      default: // DO NOTHING
     }
-    return message
-  } else {
-    console.log('Profile ID was not ' + wantedProfileId, message)
+    if (wantedMasterBrands.includes(masterBrand)) {
+      console.log('wanted masterbrand')
+      try {
+        await transport(sns.event_name, sns.uri, pid)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  } catch (error) {
+    console.log('Error! ', error)
   }
+  return undefined
 }
--- index.test.js
+++ index.test.js
@@ -1,47 +1,48 @@
-const uut = require("./index");
+/* eslint-disable no-undef */
+const uut = require('./index')
 
-test("handler", async () => {
-  const r = await uut.handler(event, context);
-  expect(r).toBe(undefined);
-});
+test('handler', async () => {
+  const r = await uut.handler(event, context)
+  expect(r).toBe(undefined)
+})
 var event = {
   Records: [
     {
-      EventSource: "aws:sns",
-      EventVersion: "1.0",
+      EventSource: 'aws:sns',
+      EventVersion: '1.0',
       EventSubscriptionArn:
-        "arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82:01ea9877-bbbf-47af-a8eb-6d013f9f44ea",
+        'arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82:01ea9877-bbbf-47af-a8eb-6d013f9f44ea',
       Sns: {
-        Type: "Notification",
-        MessageId: "e98342c1-fdea-5a5f-b155-14b5db1261f5",
+        Type: 'Notification',
+        MessageId: 'e98342c1-fdea-5a5f-b155-14b5db1261f5',
         TopicArn:
-          "arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82",
+          'arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82',
         Subject: null,
         Message:
           '{"content_version_id":"pips-pid-p07qtqhw","drm":"none","media_asset_id":"pips-pid-p07qtrvv","sequence_stamp":1570807107001000,"profile_id":"pips-map_id-a_od_p006","uri":"https://dazzler-test-bucket.s3-eu-west-1.amazonaws.com/av_pv10_pa4/modav/bUnknown-1ba9cc05-8aed-41cd-9a9e-8112579db9d9_w1msh390zxm7wrt_cUnknown_1576245484850.mp4","last_updated":"2019-10-11T15:18:51.380Z","event_name":"INSERT"}',
-        Timestamp: "2019-12-18T13:08:08.066Z",
-        SignatureVersion: "1",
+        Timestamp: '2019-12-18T13:08:08.066Z',
+        SignatureVersion: '1',
         Signature:
-          "LkLMPn+1iBBSb9Qf6p/ZawAq82U9OLacijbzO+DfbnVN/qQGSwzs0fnBoGfdXVO+R35wPfxhiZ+WxxIjaIrxU+wiGCqMnqzFsv8dP/W2qwaeahshDxIqFCOKW7ytDMy60fr8OiAwtob996miwCj5hRlh3teOlv7zNhzvjptiN5Jsbsl28YRqcZykpaboN8xLeKlw3fgL9fFvqiIX1owrGzMgR3cOP8d+YgxEEifdXNztCSkriz0+sMcYi4GJDezAVO5Hz3lPNS8iy+VMHfCS6BOU8wsTT8fwbZfSF9zNKacugpu7w/mhxEjkNnBdFfFZ2e+abjecv0uAeXyRhBdO7g==",
+          'LkLMPn+1iBBSb9Qf6p/ZawAq82U9OLacijbzO+DfbnVN/qQGSwzs0fnBoGfdXVO+R35wPfxhiZ+WxxIjaIrxU+wiGCqMnqzFsv8dP/W2qwaeahshDxIqFCOKW7ytDMy60fr8OiAwtob996miwCj5hRlh3teOlv7zNhzvjptiN5Jsbsl28YRqcZykpaboN8xLeKlw3fgL9fFvqiIX1owrGzMgR3cOP8d+YgxEEifdXNztCSkriz0+sMcYi4GJDezAVO5Hz3lPNS8iy+VMHfCS6BOU8wsTT8fwbZfSF9zNKacugpu7w/mhxEjkNnBdFfFZ2e+abjecv0uAeXyRhBdO7g==',
         SigningCertUrl:
-          "https://sns.eu-west-1.amazonaws.com/SimpleNotificationService-6aad65c2f9911b05cd53efda11f913f9.pem",
+          'https://sns.eu-west-1.amazonaws.com/SimpleNotificationService-6aad65c2f9911b05cd53efda11f913f9.pem',
         UnsubscribeUrl:
-          "https://sns.eu-west-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82:01ea9877-bbbf-47af-a8eb-6d013f9f44ea",
+          'https://sns.eu-west-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:294595276257:LiveMsdrPercolatorWorldServicePartnersResources-NotificationsTopic-10ZP19CXN5F82:01ea9877-bbbf-47af-a8eb-6d013f9f44ea',
         MessageAttributes: {}
       }
     }
   ]
-};
+}
 var context = {
   callbackWaitsForEmptyEventLoop: true,
   logGroupName:
-    "/aws/lambda/MediaSyndicationApiNotificationsHandlerForPartners",
-  logStreamName: "2019/12/17/[$LATEST]f2b113501a774ddb97437e9d18fc6358",
-  functionName: "MediaSyndicationApiNotificationsHandlerForPartners",
-  memoryLimitInMB: "128",
-  functionVersion: "$LATEST",
-  invokeid: "3191d65c-5040-4743-8826-0379b2b9059f",
-  awsRequestId: "3191d65c-5040-4743-8826-0379b2b9059f",
+    '/aws/lambda/MediaSyndicationApiNotificationsHandlerForPartners',
+  logStreamName: '2019/12/17/[$LATEST]f2b113501a774ddb97437e9d18fc6358',
+  functionName: 'MediaSyndicationApiNotificationsHandlerForPartners',
+  memoryLimitInMB: '128',
+  functionVersion: '$LATEST',
+  invokeid: '3191d65c-5040-4743-8826-0379b2b9059f',
+  awsRequestId: '3191d65c-5040-4743-8826-0379b2b9059f',
   invokedFunctionArn:
-    "arn:aws:lambda:eu-west-1:205979497597:function:MediaSyndicationApiNotificationsHandlerForPartners"
-};
+    'arn:aws:lambda:eu-west-1:205979497597:function:MediaSyndicationApiNotificationsHandlerForPartners'
+}
